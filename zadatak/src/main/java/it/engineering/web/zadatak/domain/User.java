823d31b4594208c7f089e3bec8f3ea795d84a3e9
Deni Osmani <deni.osmani098@gmail.com>
2022-04-14 10:04:46 +0200

    "user"

--- zadatak/src/main/java/it/engineering/web/zadatak/domain/User.java
+++ zadatak/src/main/java/it/engineering/web/zadatak/domain/User.java
@@ -2,8 +2,50 @@ package it.engineering.web.zadatak.domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User implements Serializable{
+	
+	@Id
+	@Column(name = "IdU")
 	private int idU;
+	@Column(name = "username")
 	private String username;
+	@Column(name="password")
 	private String password;
+	public User(int idU, String username, String password) {
+		super();
+		this.idU = idU;
+		this.username = username;
+		this.password = password;
+	}
+	public User() {
+		super();
+	}
+	public int getIdU() {
+		return idU;
+	}
+	public void setIdU(int idU) {
+		this.idU = idU;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "User [idU=" + idU + ", username=" + username + ", password=" + password + "]";
+	}
+	
 }
