e6a9013807d65d7d2a21af5a7d826a9188df6da2
Deni Osmani <deni.osmani098@gmail.com>
2022-04-16 12:53:49 +0200

    logout addition

--- zadatak/src/main/java/it/engineering/web/zadatak/action/login/LogoutAction.java
+++ zadatak/src/main/java/it/engineering/web/zadatak/action/login/LogoutAction.java
@@ -1,11 +1,14 @@
 package it.engineering.web.zadatak.action.login;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
 import it.engineering.web.zadatak.action.AbstractAction;
+import it.engineering.web.zadatak.domain.User;
 
 
 
@@ -14,6 +17,18 @@ public class LogoutAction extends AbstractAction {
 	@Override
 	public String executeRequest(HttpServletRequest request, HttpServletResponse response) {
 		HttpSession session = request.getSession();
+		User user=(User)session.getAttribute("loginUser");
+		List<User> list=(List<User>) request.getServletContext().getAttribute("logedUsers");
+		int i=0;
+		boolean ima=false;
+		for(User current:list) {
+			if(current.equals(user)) {ima=true; break;}
+			i++;
+		}
+		if(ima) {
+			list.remove(i);
+			System.out.println(list);
+		}
 		session.removeAttribute("loginUser");
 		session.invalidate();
 		session = request.getSession(false);
